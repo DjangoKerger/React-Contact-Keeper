d76e4c3f97a28ee82f897785bf110024ee53aee9
“First <Kerger06@aol.com>
2020-10-01 17:19:27 -0500

    user model and data validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+//bring in mongoose schema
+const User = require('../models/User');
 
 // @route       POST api/users
 //@ description Register a user
 //@access       Public
-router.post('/', (req, res)=> {
-    res.send('Register a user');
-});
+
+//post request with all the required checks, if checks are not passed 
+//server will return status 400
+router.post(
+    '/', 
+    [
+        check('name', 'Name is required')
+            .not()
+            .isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'please enter a password with 6 or more characters')
+        .isLength({ min: 6})
+], 
+(req, res)=> {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array() });
+    }
+    res.send('passed')
+
+}
+);
 
 module.exports = router;
