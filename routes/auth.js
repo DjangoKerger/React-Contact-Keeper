c72557ea32d1a0a832085e8f3178adc083f96d8e
“First <Kerger06@aol.com>
2020-10-06 10:50:55 -0500

    added authorization middleware to validate webtoken

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth'); //pass as second parameter to protect route
 const { check, validationResult } = require('express-validator');
 
 //bring in mongoose schema
@@ -11,8 +12,15 @@ const User = require('../models/User');
 // @route       GET api/auth
 //@ description Get logged in user
 //@access       Private
-router.get('/', (req, res)=> {
-    res.send('Get logged in user');
+router.get('/', auth, async (req, res)=> {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+    
 });
 
 
