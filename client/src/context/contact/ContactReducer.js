c93bb0dbb130a1ddd650d571597ece5103a0f471
“First <Kerger06@aol.com>
2020-10-10 12:38:34 -0500

    set and clear contact ui

--- client/src/context/contact/ContactReducer.js
+++ client/src/context/contact/ContactReducer.js
@@ -19,6 +19,17 @@ export default (state, action) => {
             return {
                 ...state,
                 contacts: state.contacts.filter(contact => contact.id !== action.payload)
+            };
+        case SET_CURRENT:
+            return {
+                ...state,
+                current: action.payload
             }
+            case CLEAR_CURRENT:
+                return {
+                    ...state,
+                    current: null
+                }    
+        default: return state;        
     }
-}
+};
