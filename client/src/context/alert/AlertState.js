2ce754507dad7108d47f784e200da470c8a426ba
“First <Kerger06@aol.com>
2020-10-14 10:34:46 -0500

    ability to register user added along with error messages in case user already exists

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -2,7 +2,8 @@ import React, { useReducer } from 'react';
 import AlertContext from './alertContext';
 import alertReducer from './alertReducer';
 import {
-    SET_ALERT, REMOVE_ALERT
+    SET_ALERT, 
+    REMOVE_ALERT
 } from '../types';
 
 const AlertState = props => {
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
 import {
@@ -26,13 +27,40 @@ const AuthState = props => {
 
     //Load User
 
+    const loadUser = () => console.log('load user');
+
     //Register User
+    const register = async formData => {
+        const config = {
+            headers: {
+                'Content-type': 'application/json'
+            }
+        };
+    
+
+    try {
+        const res = await axios.post('api/users', formData, config);
+
+        dispatch({
+            type: REGISTER_SUCCESS,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: REGISTER_FAIL,
+            payload: err.response.data.msg
+            })
+        }
+    };
 
     //Login User
+    const login = () => console.log('login user');
 
     //Logout
+    const logout = () => console.log('logout user');
 
     //Clear Errors
+    const clearErrors = () =>dispatch( {type: CLEAR_ERRORS} );
 
 
     return (
@@ -42,7 +70,13 @@ const AuthState = props => {
            isAuthenticated: state.isAuthenticated,
            loading: state.loading,
            user: state.user,
-           error: state.error
+           error: state.error,
+           register,
+           loadUser,
+           login,
+           logout,
+           clearErrors
+
         }}>
         
         {props.children}
